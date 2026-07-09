310de1a58d23c897444038535097e1a902cf3a80
kaveh.taqipour <kavehtaqipour@mail.com>
2026-07-09 10:34:24 +0330

    Implement custom user model with email authentication

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustumUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+
+    ordering = ("email", )
+    search_fields = ("email", )
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("permissions", {"fields": ("is_staff", "is_active",
+         "is_superuser", "groups", "user_permissions")}),
+        ("important dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active")
+        }),
+    )
