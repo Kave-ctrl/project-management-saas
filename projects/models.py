29552cafa4df39860e3c4a05d21f9ef4da5db42a
kaveh.taqipour <kavehtaqipour@mail.com>
2026-08-31 15:20:20 +0330

    Add domain validations for org membership on Project and Task

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
 
 
 class Project(models.Model):
@@ -26,3 +27,13 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+    def clean(self):
+        if self.created_by_id is not None:
+            if not self.organization.members.filter(id=self.created_by_id).exists():
+                raise ValidationError(
+                    {"created_by": "created_by must be a member of the organization."})
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
 
 
 class Task(models.Model):
@@ -56,6 +57,24 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+    def clean(self):
+        org = self.project.organization
+
+        # created_by must be member of the project's organization (if provided)
+        if self.created_by_id is not None:
+            if not org.members.filter(id=self.created_by_id) .exists():
+                raise ValidationError({"created_by": "created_by must be a member of the project's organization."})
+
+        # assigned_to must be member of the project's organization (if provided)
+        if self.assigned_to_id is not None:
+            if not org.members.filter(id=self.assigned_to_id) .exists():
+                raise ValidationError(
+                    {"assigned_to": "assigned_to must be a member of the project's organization"})
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # runs clean() + field validation
+        return super().save(*args, **kwargs)
 
 
 class TaskComment(models.Model):
